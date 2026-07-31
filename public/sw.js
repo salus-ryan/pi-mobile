04402189545698cf567065022aa1be8b633ce01c
salus-ryan <salus-ryan@users.noreply.github.com>
2026-07-31 08:00:51 -0400

    Bust cached assets for math renderer

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,13 @@
-const CACHE = "pi-mobile-v1";
-const SHELL = ["/", "/styles.css", "/app.js", "/manifest.webmanifest", "/icon.svg"];
+const CACHE = "pi-mobile-v2";
+const SHELL = [
+  "/",
+  "/styles.css?v=2",
+  "/app.js?v=2",
+  "/vendor/katex/katex.min.css?v=2",
+  "/vendor/katex/katex.min.js?v=2",
+  "/manifest.webmanifest",
+  "/icon.svg",
+];
 self.addEventListener("install", (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(SHELL))));
 self.addEventListener("activate", (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key))))));
 self.addEventListener("fetch", (event) => {
